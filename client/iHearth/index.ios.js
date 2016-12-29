8faa74f068eb8c6c56422fd8c7678d02e34d8485
jamesgu <jamesgu521@gmail.com>
2016-12-29 02:13:12 -0800

    (fix) Save react-redux dependency and bugfix in index

--- client/iHearth/index.ios.js
+++ client/iHearth/index.ios.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 
-import configureStore from './app/store/configureStore';
+import configureStore from './App/store/configureStore';
 const store = configureStore();
 
-import NavRootContainer from './app/containers/NavRootContainer';
+import NavRootContainer from './App/containers/NavRootContainer';
 import { Provider } from 'react-redux';
 
 const iHearth = () => (
