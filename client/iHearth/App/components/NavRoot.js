abf7c9c9e98f0e2ac6c2a904b429978c5d464c31
Hong <keepthemonochrome@gmail.com>
2016-12-29 00:47:40 -0800

    (feature) Set up for NavigationExperimental, Redux - part 2

---
+++ client/iHearth/App/components/NavRoot.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ListView from './ListView';
+import CouponView from './CouponView';
+
+import {
+  // BackAndroid,
+  NavigationExperimental
+} from 'react-native';
+
+const {
+  CardStack: NavigationCardStack
+} = NavigationExperimental;
+
+class NavRoot extends Component {
+  constructor(props) {
+    super(props);
+    this._renderScene = this._renderScene.bind(this);
+    this._handleBackAction = this._handleBackAction.bind(this);
+  }
+
+  componentDidMount(){
+    //BackAndroid
+  }
+
+  componentWillUnmount(){
+    //
+  }
+
+  _renderScene(props) {
+    const { route } = props.scene;
+    switch (route.key) {
+      case 'ListView':
+        return (
+          <ListView _handleNavigate= { this._handleNavigate.bind(this) } />
+        )
+      case 'CouponView':
+        return (
+          <CouponView _goBack={ this._handleBackAction.bind(this) } />
+        )
+    }
+  }
+}
