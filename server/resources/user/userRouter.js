c568fc6ae84c464320a10817944ea185f551ced2
pengjoshua <joshuapeng7@gmail.com>
2016-12-31 18:20:22 -0800

    (feat/routes) Add routes to userController/userRouter

--- server/resources/user/userRouter.js
+++ server/resources/user/userRouter.js
@@ -1,18 +1,24 @@
 var express = require('express');
-//var userCtrl = require('../controllers/userController');
-//var boopHelper = require('../boopData/boopHelper.js');
+var userController = require('../controllers/userController');
 var app = express();
 var userRouter = require('express').Router();
 
+userRouter.get('/user', userController.retrieveUsers);
+userRouter.post('/user', userController.createUser);
+
+userRouter.get('/coupon/:coupon_id', userController.retrieveOneCoupon);
+userRouter.get('/coupon', userController.retrieveCoupons);
+userRouter.post('/coupon', userController.createCoupon);
+
+userRouter.get('/user/coupon', userController.retrieveUserCoupons);
+userRouter.post('/user/coupon', userController.sendUserCoupons);
+
+userRouter.get('/user/coupon/:coupon_id', userController.retrieveOneUserCoupon);
+userRouter.post('/user/coupon/:coupon_id', userController.createUserCoupon);
+userRouter.put('/user/coupon/:coupon_id', userController.useUserCoupon);
+
 userRouter.get('/login', userController.login);
-userRouter.get('/user/coupon', userController.userCoupon);
-userRouter.get('/user/coupon/:id', userController.retrieveOneCoupon);
-userRouter.post('/user/coupon', userController.sendCouponToUser);
-
 userRouter.post('/signup', userController.signup);
-userRouter.post('/userCoupon', userController.retrieveCoupons);
-userRouter.post('/logout', userController.logout)
-
-userRouter.put('/user/coupon/:id', userController.useCoupon);
+userRouter.post('/logout', userController.logout);
 
 module.exports = userRouter;
