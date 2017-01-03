dfd648108e564e5f22172a7bcfcc82d07139733c
blakeFleck <fleckblake4@gmail.com>
2017-01-03 13:34:58 -0800

    working on sign in and login views

--- client/iHearth/App/components/LoginView.js
+++ client/iHearth/App/components/LoginView.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   View,
-  Text
+  Text,
+  TextInput
 } from 'react-native'
 import Button from './global-components/Button'
 import styles from './../styles';
@@ -23,12 +24,50 @@ const authRoute = {
   }
 };
 
-const LoginView = ({ _handleNavigate }) => (
-  <View style={ styles.container }>
-    <Text style={ styles.title }>LoginView</Text>
-    <Button onPress={ () => _handleNavigate(route) } label='New User? Signup!' />
-    <Button onPress={ () => _handleNavigate(authRoute) } label="Gather 'Round the Hearth" />
-  </View>
-);
+class LoginView extends Component {
+  constructor(props){
+    super(props);
+    this.state = { email: 'Please Enter Email', password: 'Please Enter Password' };
+  }
+  // handleLogin = () => {
+  //   var context = this;
+  //   var returningUser = {
+  //     email: this.state.email,
+  //     password: this.state.password
+  //   };
+  //   loginCtrl.login(returningUser)
+  //   .then(function(res) {
+  //     context.setState({loginIsOpen: false});
+  //     context.props.main.setState({isLoggedIn: true});
+  //     // plugs in email and password in an object into dispatch
+  //     context.props.dispatch(logUser(res.data.userData));
+  //   })
+  //   .catch(err => {
+  //     const msg = err.response.data.message;
+  //     this.setState({ submitError: msg });
+  //   })
+  // };
+  render() {
+   return(
+      <View style={ styles.container }>
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({email})}
+        onFocus= {() => this.setState({email : ''})}
+        value={this.state.email}
+      />
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({password})}
+        onFocus= {() => this.setState({password : ''})}
+        value={this.state.password}
+      />
+        <Text style={ styles.title }>LoginView</Text>
+        <Button onPress={ () => this.props._handleNavigate(route) } label='New User? Signup!' />
+        <Button onPress={ () => this.props._handleNavigate(authRoute) } label="Gather 'Round the Hearth" />
+      </View>
+   )
+  }
+}
 
 export default LoginView;
