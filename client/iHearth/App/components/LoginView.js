4e1d28485f76deefd91a3cb935cc21abfe349e66
blakeFleck <fleckblake4@gmail.com>
2017-01-03 16:02:44 -0800

    working on the login/signin view

--- client/iHearth/App/components/LoginView.js
+++ client/iHearth/App/components/LoginView.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {
   View,
-  Text,
-  TextInput
+  TextInput,
+  Text
 } from 'react-native'
 import Button from './global-components/Button'
+import { Container, Content, List, ListItem, InputGroup, Input, Icon, Picker } from 'native-base';
 import styles from './../styles';
+
+const Item = Picker.Item;
 
 const route = {
   type: 'push',
@@ -27,45 +30,53 @@ const authRoute = {
 class LoginView extends Component {
   constructor(props){
     super(props);
-    this.state = { email: 'Please Enter Email', password: 'Please Enter Password' };
-  }
-  // handleLogin = () => {
-  //   var context = this;
-  //   var returningUser = {
-  //     email: this.state.email,
-  //     password: this.state.password
-  //   };
-  //   loginCtrl.login(returningUser)
-  //   .then(function(res) {
-  //     context.setState({loginIsOpen: false});
-  //     context.props.main.setState({isLoggedIn: true});
-  //     // plugs in email and password in an object into dispatch
-  //     context.props.dispatch(logUser(res.data.userData));
-  //   })
-  //   .catch(err => {
-  //     const msg = err.response.data.message;
-  //     this.setState({ submitError: msg });
-  //   })
-  // };
+    this.state = {
+            email: '',
+            password: '',
+  };
+}
+ handleLogin = () => {
+    var context = this;
+    var returningUser = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    loginCtrl.login(returningUser)
+    .then(function(res) {
+      context.setState({loginIsOpen: false});
+      context.props.main.setState({isLoggedIn: true});
+      // plugs in email and password in an object into dispatch
+      context.props.dispatch(logUser(res.data.userData));
+    })
+    .catch(err => {
+      const msg = err.response.data.message;
+      this.setState({ submitError: msg });
+    })
+  };
   render() {
+    console.log(this.state.email)
    return(
-      <View style={ styles.container }>
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({email})}
-        onFocus= {() => this.setState({email : ''})}
-        value={this.state.email}
-      />
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({password})}
-        onFocus= {() => this.setState({password : ''})}
-        value={this.state.password}
-      />
-        <Text style={ styles.title }>LoginView</Text>
-        <Button onPress={ () => this.props._handleNavigate(route) } label='New User? Signup!' />
-        <Button onPress={ () => this.props._handleNavigate(authRoute) } label="Gather 'Round the Hearth" />
-      </View>
+     <Container>
+                <Content>
+                <Text style={ styles.title }>LoginView</Text>
+                    <List style={styles.login}>
+                        <ListItem>
+                            <InputGroup>
+                                <Icon name="ios-person" style={{ color: '#0A69FE' }} />
+                                <Input placeholder="Email" value={this.state.email}  onChangeText={(text) => this.setState({email:text})} />
+                            </InputGroup>
+                        </ListItem>
+                        <ListItem>
+                            <InputGroup>
+                                <Icon name="ios-unlock" style={{ color: '#0A69FE' }} />
+                                <Input placeholder="Password" value={this.state.password}  onChangeText={(text) => this.setState({password:text})} secureTextEntry />
+                            </InputGroup>
+                        </ListItem>
+                    </List>
+                  <Button onPress={ () => this.props._handleNavigate(route) } label='New User? Signup!' />
+                  <Button onPress={ () => this.props._handleNavigate(authRoute) } label="Gather 'Round the Hearth" />
+                </Content>
+            </Container>
    )
   }
 }
