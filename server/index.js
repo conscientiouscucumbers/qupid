ee92a9d6270bf66fb9171f447c918e139468d9ad
jamesgu <jamesgu521@gmail.com>
2017-01-06 00:13:47 -0800

    (fix) Configure port to dynamically listen on given

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 var app = require('./server.js');
 var port = 3000;
 
-app.listen(port, function () {
-  console.log('iHearth RESTful API listening on port ' + port);
+// app.listen(port, function () {
+//   console.log('iHearth RESTful API listening on port ' + port);
+// });
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
