39a8ba2e326cd003a6c58f3eb18306ed39692b43
jamesgu <jamesgu521@gmail.com>
2017-01-06 00:25:51 -0800

    (fix) Configure package.json and port number

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ var port = 3000;
 //   console.log('iHearth RESTful API listening on port ' + port);
 // });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 4568, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
