0db61a3875c392defa40ff80f67b32f103827014
Hong <keepthemonochrome@gmail.com>
2017-01-07 14:53:20 -0800

    (feat/beacon) Migrate beacon listener to tabsRoot

--- client/iHearth/App/components/nav-components/TabsRoot.js
+++ client/iHearth/App/components/nav-components/TabsRoot.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 import { TabBarIOS } from 'react-native';
-
 import Home from '../../containers/nav-containers/NavRootContainer';
 import Settings from '../../components/Settings';
 
+// BeaconListener
+import Beacons from 'react-native-ibeacon';
+import { DeviceEventEmitter } from 'react-native';
+
 class Tabs extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      beacons: []
+    };
+  }
+  componentDidMount() {
+    const region = {
+      identifier: 'Estimotes',
+      uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
+    };
+    Beacons.requestWhenInUseAuthorization();
+    Beacons.startRangingBeaconsInRegion(region);
+    Beacons.startUpdatingLocation();
+    DeviceEventEmitter.addListener(
+      'beaconsDidRange',
+      (data) => {
+        // console.log(data);
+        if(data['beacons'].length!==0){
+
+          console.log(data['beacons'][0]['proximity']);
+          if(data['beacons'][0]['proximity'] === 'far' || data['beacons'][0]['proximity'] === 'near'){
+            console.log('got you!');
+            Beacons.stopUpdatingLocation(); // doesn't seem like it's working.. but it's okay for now.
+          }
+        }
+      }
+    );
+  }
+
   _changeTab(i) {
     const { changeTab } = this.props;
     changeTab(i);
