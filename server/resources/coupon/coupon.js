ab61296256cd9d600874de05a6c0365bf1fb6991
pengjoshua <joshuapeng7@gmail.com>
2017-01-07 14:40:51 -0800

    (feat/coupon) Update coupon routes for qrcode

--- server/resources/coupon/coupon.js
+++ server/resources/coupon/coupon.js
@@ -2,7 +2,7 @@ var Promise = require('bluebird');
 var db = require('../../db');
 
 var retrieveCoupons = callback => {
-  var queryStr = 'select c.coupon_id, c.business_id, c.title, c.image, c.item_name, \
+  var queryStr = 'select c.coupon_id, c.business_id, c.qrcode, c.title, c.image, c.item_name, \
   c.description, c.original_price, c.coupon_price, c.coupon_savings, CAST(c.start_at as time) as start_at, \
   CAST(c.end_at as time) as end_at, CAST(c.created_at as time) as created_at, b.company_name from coupon as c left join business as b on \
   c.business_id = b.business_id';
@@ -18,8 +18,8 @@ var retrieveCoupons = callback => {
 module.exports.retrieveCouponsAsync = Promise.promisify(retrieveCoupons);
 
 var createCoupon = (params, callback) => {
-  var queryStr = `insert into coupon (title, image, item_name, description, original_price, coupon_price, coupon_savings, start_at, end_at) \
-    values ("${params.title}", "${params.image}", "${params.item_name}", "${params.description}", "${params.original_price}", "${params.coupon_price}", "${params.coupon_savings}", "${params.start_at}", "${params.end_at}")`;
+  var queryStr = `insert into coupon (qrcode, title, image, item_name, description, original_price, coupon_price, coupon_savings, start_at, end_at) \
+    values ("${params.qrcode}", "${params.title}", "${params.image}", "${params.item_name}", "${params.description}", "${params.original_price}", "${params.coupon_price}", "${params.coupon_savings}", "${params.start_at}", "${params.end_at}")`;
   db.query(queryStr, function(err, res) {
 
     if (err) {
