3eb7b0c50fb5a71a7e2ce981d15cec08adcfe7fd
pengjoshua <joshuapeng7@gmail.com>
2017-01-07 16:36:37 -0800

    (feat/buttons) cool buttons

--- client/iHearth/App/components/global-components/Button.js
+++ client/iHearth/App/components/global-components/Button.js
@@ -1,11 +1,30 @@
+// import React from 'react';
+// import { Text, TouchableHighlight } from 'react-native';
+// import styles from './../../styles';
+//
+// export default ({ label, onPress }) => (
+//   <TouchableHighlight
+//     underlayColor='#35b5ff'
+//     onPress={ onPress } style={ styles.button }>
+//     <Text style={ styles.label }>{ label }</Text>
+//   </TouchableHighlight>
+// );
+
 import React from 'react';
 import { Text, TouchableHighlight } from 'react-native';
 import styles from './../../styles';
+import ButtonComponent, { CircleButton, RoundButton, RectangleButton } from 'react-native-button-component';
 
 export default ({ label, onPress }) => (
-  <TouchableHighlight
-    underlayColor='#35b5ff'
-    onPress={ onPress } style={ styles.button }>
-    <Text style={ styles.label }>{ label }</Text>
-  </TouchableHighlight>
+  <ButtonComponent
+    type="primary"
+    shape="rectangle"
+    backgroundColors={['#4DC7A4', '#66D37A']}
+    gradientStart={[0.5, 1]}
+    gradientEnd={[1, 1]}
+    height={80}
+    onPress={ onPress }
+    text={ label }
+  >
+  </ButtonComponent>
 );
