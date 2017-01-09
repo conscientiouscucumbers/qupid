f0961127d10322af1dd7308cb7b1365f8257bdc8
Hong <keepthemonochrome@gmail.com>
2017-01-08 20:29:22 -0800

    (feat/alert) Alert window popup when coupon sent by beacon

--- client/iHearth/App/components/ListView.js
+++ client/iHearth/App/components/ListView.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text
-} from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import ListViewEntry from './ListViewEntry';
 import styles from './../styles';
-import {
-  Container,
-  List,
-  Content,
-} from 'native-base';
+import { Container, List, Content } from 'native-base';
+import DropdownAlert from 'react-native-dropdownalert'
 
 const route = {
   type: 'push',
@@ -23,6 +17,7 @@ export default class ListView extends Component {
   constructor(props) {
     super(props);
     _handleNavigate = this.props._handleNavigate;
+    this.showAlert = this.showAlert.bind(this);
   }
 
   // Before rendering, get state from server
@@ -33,11 +28,22 @@ export default class ListView extends Component {
     this.props.fetchCoupons(this.user_id);
   }
 
+  componentDidMount() {
+    // if (this.props.pushedCoupons.pushedCoupons[0]) {
+    //   this.showAlert('custom');
+    //   console.log('showing alert!');
+    // }
+
+    if(this.props.pushedCoupons.pushedCoupons[0])
+      console.log('====TITLE'+ this.props.pushedCoupons.pushedCoupons[0].title);
+    else
+      console.log("null");
+  }
+
   render() {
-    console.log('LISTVIW PROPS...', this.props.pushedCoupons);
-    // let temp;
-    // this.props.pushedCoupons.pushedCoupons[0] === undefined? temp="null" : temp=this.props.pushedCoupons.pushedCoupons[0].title
-    // console.log(temp);
+    // console.log('LISTVIW PROPS...', this.props.pushedCoupons);
+    // console.log('CHANGES?');
+    this.props.pushedCoupons.pushedCoupons[0] ? this.showAlert('custom') : null
     return (
       <View>
         <Text>{this.props.pushedCoupons.pushedCoupons[0] ? this.props.pushedCoupons.pushedCoupons[0].title : "null" }</Text>
@@ -49,7 +55,63 @@ export default class ListView extends Component {
               coupon={ coupon } />
           ))}
         </List>
+        <DropdownAlert
+          ref={(ref) => this.dropdown = ref}
+          titleNumOfLines={1}
+          messageNumOfLines={5}
+          closeInterval={0}
+          containerStyle={{
+            backgroundColor: "#6441A4",
+            margin: 30,
+            borderRadius: 8
+          }}
+          onClose={(data) => this.onClose(data)}
+          onCancel={(data) => this.onClose(data)}
+          showCancel={true}
+          imageSrc={'https://facebook.github.io/react/img/logo_og.png'}
+        />
       </View>
     );
   }
+
+  showAlert(type) {
+    // customize alert window if more types needed
+     // data = {type, title, message}
+     switch (type) {
+      case 'custom':
+        this.dropdown.alert('A new coupon has arrived!', this.props.pushedCoupons.pushedCoupons[0].title);
+        // this.dropdown.alert('Custom','A new coupon has arrived!', 'eyy');
+        // this.dropdown.alertWithType(type, 'Error', 'A new coupon has arrived!', 'eyy');
+      break;
+    }
+
+  }
+  closeAlert() {
+    this.dropdown.onClose()
+  }
+  onClose(data) {
+    console.log(data)
+  }
 }
+
+const styles2 = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection: 'column',
+    // justifyContent: 'center',
+    // alignItems:'center'
+    // backgroundColor: 'honeydew'
+  },
+  contentContainer: {
+    marginTop: 22,
+
+  },
+  text: {
+    fontFamily: 'Avenir',
+    fontWeight: 'bold',
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'white'
+  },
+});
+
