8ba3487c9cef22ce50e1125ffa8cd3826e0efddb
jamesgu <jamesgu521@gmail.com>
2017-01-09 22:55:16 -0800

    (feat) Add spinner for fetching coupons

--- client/iHearth/App/constants/NetworkUrls.js
+++ client/iHearth/App/constants/NetworkUrls.js
@@ -1,2 +1,2 @@
-export const URL = 'http://127.0.0.1:4568/';
-// export const URL = 'https://quiet-beyond-88440.herokuapp.com/';
+// export const URL = 'http://127.0.0.1:4568/';
+export const URL = 'https://quiet-beyond-88440.herokuapp.com/';
