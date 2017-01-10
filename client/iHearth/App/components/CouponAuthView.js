b01c73dac408466fcd60ec78b5a1715916bab543
pengjoshua <joshuapeng7@gmail.com>
2017-01-09 19:07:55 -0800

    (feat/ui) updated UI for qrcode and coupon views

--- client/iHearth/App/components/CouponAuthView.js
+++ client/iHearth/App/components/CouponAuthView.js
@@ -1,12 +1,12 @@
 //Coupon Auth View not triggered from CouponDescrption yet
 import React from 'react';
-import { Container, Header, Icon, Title, Content, Text } from 'native-base';
+import { Container, Header, Icon, Title, Content, Text, Button } from 'native-base';
 
 const CouponAuthView = () => (
   <Container>
     <Header>
-      <Button transparent>
-          <Icon name='ios-arrow-back' />
+      <Button transparent onPress={ _goBack }>
+        <Icon name='ios-arrow-back' />
       </Button>
       <Title>Use Selected Coupon</Title>
     </Header>
