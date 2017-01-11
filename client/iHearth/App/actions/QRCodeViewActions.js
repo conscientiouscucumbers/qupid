f65cc511ebcb1273abe4d3b1b487ca321feac138
jamesgu <jamesgu521@gmail.com>
2017-01-10 17:36:09 -0800

    (feat) Autorender listView on coupon use

--- client/iHearth/App/actions/QRCodeViewActions.js
+++ client/iHearth/App/actions/QRCodeViewActions.js
@@ -1,6 +1,6 @@
 import { REQUEST_USE_COUPON, RECEIVE_USE_COUPON } from '../constants/ActionTypes';
 import { URL } from '../constants/NetworkUrls';
-
+import { fetchPosts } from '../actions/listViewActions'
 // Will change isFetching state of list to true
 function requestUseCoupon() {
   return {
@@ -29,7 +29,10 @@ export function useCoupon(user_id, coupon_id) {
     return fetch(request)
       .then(response => response.json())
       .then(json => {
-          console.log('SUCCESSFULLY UPDATED ENTRY');
+          // Dispatch to update state of listView
+          dispatch(fetchPosts(user_id));
+
+          // Dispatch to update QR reducer state
           return dispatch(receiveUseCoupon(json));
         })
       .catch((err) => {
