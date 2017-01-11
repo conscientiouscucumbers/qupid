e9bec92df3ef30aea4bb484dd8982523dd42ed17
jamesgu <jamesgu521@gmail.com>
2017-01-10 21:31:03 -0800

    (feat) Start implementing worker

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+// Cron module
+var schedule = require('node-schedule');
+
 var app = require('./server.js');
 var port = 3000;
 
@@ -7,4 +10,9 @@ var port = 3000;
 
 app.listen(process.env.PORT || 4568, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  
+  // Schedule job every second
+  // var j = schedule.scheduleJob({ rule: '*/5 * * * * *' }, function() {
+  //   console.log('Time for tea!');
+  // });
 });
