e3b8d3c0ab99cece9b84aa5f14934e6804e303cf
pengjoshua <joshuapeng7@gmail.com>
2017-01-10 16:23:45 -0800

    (feat/hearts) add explodinghearts component on loginview

--- client/iHearth/App/components/LoginView.js
+++ client/iHearth/App/components/LoginView.js
@@ -76,6 +76,7 @@ export default class LoginView extends Component {
           <List style={{ marginTop: 10 }}>
             <Button onPress={ () => this.props.fetchAuth(this.state, authRoute, this.props._handleNavigate) } label="Log In" />
           </List>
+          <ExplodingHearts />
         </View>
       </LinearGradient>
     )
@@ -84,7 +85,7 @@ export default class LoginView extends Component {
 
 var styles = StyleSheet.create({
   title: {
-    paddingTop: 160,
+    paddingTop: 140,
     fontSize: 44,
     fontWeight: '100',
     textAlign: 'center',
