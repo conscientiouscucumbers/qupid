bad1c61b355f5f8e713a036a13a21d9670501092
pengjoshua <joshuapeng7@gmail.com>
2017-01-12 15:11:28 -0800

    (feat/sql) changes to dummydate

--- server/db/dummydata.sql
+++ server/db/dummydata.sql
@@ -23,13 +23,13 @@ values ('beardpapas@gmail.com', 'password', 'Beard Papas', '744 Market Street', 
 
 -- // coupon
 insert into coupon (business_id, qrcode, title, image, item_name, description, original_price, coupon_price, coupon_savings, start_at, end_at)
-values (1, 'qrcode1', '$5 OFF Hoodie', 'https://facebook.github.io/react/img/logo_og.png', 'Men\'s Half Dome Hoodie', 'Now crafted with an updated fit, this comfy pullover hooded sweatshirt is finished with athletic-style ribbed cuffs and hem.', 10.00, 5.00, 5.00, '2017-01-05 16:00:00', '2017-01-10 21:25:00');
+values (1, 'qrcode1', '$5 OFF Hoodie', 'https://facebook.github.io/react/img/logo_og.png', 'Men\'s Half Dome Hoodie', 'Now crafted with an updated fit, this comfy pullover hooded sweatshirt is finished with athletic-style ribbed cuffs and hem.', 10.00, 5.00, 5.00, '2017-01-05 16:00:00', '2017-01-15 21:25:00');
 
 insert into coupon (business_id, qrcode, title, image, item_name, description, original_price, coupon_price, coupon_savings, start_at, end_at)
-values (2, 'qrcode2', '$20 OFF Sneakers', 'https://facebook.github.io/react/img/logo_og.png', 'Men’s Berkeley Redux Sneaker', 'This casual yet warm low-cut sneaker features a waterproof construction, synthetic insulation, and a grippy rubber sole.', 120.00, 100.00, 20.00, '2017-01-05 01:00:00', '2017-01-10 21:25:00');
+values (2, 'qrcode2', '$20 OFF Sneakers', 'https://facebook.github.io/react/img/logo_og.png', 'Men’s Berkeley Redux Sneaker', 'This casual yet warm low-cut sneaker features a waterproof construction, synthetic insulation, and a grippy rubber sole.', 120.00, 100.00, 20.00, '2017-01-05 01:00:00', '2017-01-15 21:25:00');
 
 insert into coupon (business_id, qrcode, title, image, item_name, description, original_price, coupon_price, coupon_savings, start_at, end_at)
-values (3, 'qrcode3', '$4 OFF Shampoo', 'https://facebook.github.io/react/img/logo_og.png', 'Amino Acid Shampoo', 'A naturally-derived shampoo that cleanses and softens hair. Creates a rich, creamy lather for a delightful shampoo experience. A special blend of moisturizing ingredients imparts softness and shine as the formula adds body and fullness. Suitable for all hair and scalp types when mildness is desired.', 8.00, 4.00, 4.00, '2017-01-06 03:00:00', '2017-01-10 21:25:00');
+values (3, 'qrcode3', '$4 OFF Shampoo', 'https://facebook.github.io/react/img/logo_og.png', 'Amino Acid Shampoo', 'A naturally-derived shampoo that cleanses and softens hair. Creates a rich, creamy lather for a delightful shampoo experience. A special blend of moisturizing ingredients imparts softness and shine as the formula adds body and fullness. Suitable for all hair and scalp types when mildness is desired.', 8.00, 4.00, 4.00, '2017-01-06 03:00:00', '2017-01-15 21:25:00');
 
 insert into coupon (business_id, qrcode, title, image, item_name, description, original_price, coupon_price, coupon_savings, start_at, end_at)
 values (3, 'qrcode4', '$4 OFF Conditioner', 'https://facebook.github.io/react/img/logo_og.png', 'Conditioner', 'Lorem Ipsum.', 8.00, 4.00, 4.00, '2017-01-15 03:00:00', '2017-02-10 09:00:00');
