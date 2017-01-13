bd4857f8b92fe715a0c4690a1a22cb7b703f9986
pengjoshua <joshuapeng7@gmail.com>
2017-01-12 21:13:27 -0800

    (feat/login) login route works with postman

--- biz_web/api/actions/logout.js
+++ biz_web/api/actions/logout.js
@@ -5,13 +5,17 @@ export default function logout(req) {
     req.session.destroy(() => {
       req.session = null;
 
-      var queryStr = `select * from business where email = "${params.email}"`;
+      const business = {
+        email: req.body.email,
+      };
+
+      var queryStr = `select * from business where email = "${business.email}"`;
       db.query(queryStr, (err, business) => {
         if (err) {
-          console.log('could not find user in user table');
+          console.log('could not find business in business table');
           reject(err);
         } else {
-          console.log('successfully found user to log out = ', user[0]);
+          console.log('successfully found business to log out = ', business);
           return resolve(business);
         }
       });
