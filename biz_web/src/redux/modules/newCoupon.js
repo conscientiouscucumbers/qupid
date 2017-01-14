742c976763fcc88bcf0643a449ac1ae616682200
James Gu <jamesgu@Jamess-MacBook-Pro.local>
2017-01-13 21:36:58 -0800

    (feat) Finish bare minimum functionality of adding new coupons to DB

--- biz_web/src/redux/modules/newCoupon.js
+++ biz_web/src/redux/modules/newCoupon.js
@@ -10,10 +10,8 @@ export default function reducer(state = initialState, action = {}) {
   console.log('action send to reducer...', action);
   switch (action.type) {
     case IS_VALID:
-      console.log('REDUCER IS_VALID CASE');
       return state; // 'saving' flag handled by redux-form
     case IS_VALID_SUCCESS:
-      console.log('REDUCER IS_VALID_SUCCESS CASE');
       const data = [...state.data];
       data[action.result.id - 1] = action.result;
       return {
@@ -22,7 +20,7 @@ export default function reducer(state = initialState, action = {}) {
         saveError: null
       };
     case IS_VALID_FAIL:
-      console.log('REDUCER IS_VALID_FAIL CASE');
+      console.log("REDUCER IS_VALID_FAIL CASE");
       return typeof action.error === 'string'
         ? {
           ...state,
@@ -30,19 +28,16 @@ export default function reducer(state = initialState, action = {}) {
         }
         : state;
     default:
-      console.log('DEFAULT CASE IN NEWCOUPON');
       return state;
   }
 }
 
 export function isValidCoupon(data) {
-  console.log('ISVALIDCOUPON CALLED', data);
+  console.log("ISVALIDCOUPON CALLED", data);
   return {
     types: [
       IS_VALID, IS_VALID_SUCCESS, IS_VALID_FAIL
     ],
-    // data: data
-    // promise: (client) => client.post('/newcoupon/isValidNewCoupon', {data})
-    promise: (client) => client.post('/newcoupon/isValidNewCoupon', {"title":"$10 off Beard Papas","image":"https://upload.wikimedia.org/wikipedia/commons/6/64/Banana_Peel.JPG","item_name":"Cream Puff","description":"Lorem Ipsum","original_price":5,"coupon_savings":2,"start_at":"2017-05-21 12:00:00","end_at":"2017-05-21 12:00:00"})
+    promise: (client) => client.post('/newcoupon/isValidNewCoupon', {data})
   };
 }
